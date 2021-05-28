93cbed0e66473f5da4ce9b54af4227f1b6bf9f57
seizeMiss <dragon_csu@163.com>
2021-05-28 10:52:10 +0800

    解决LeetCode477习题

--- src/main/java/com/dragon/leetcode/LeetCode461.java
+++ src/main/java/com/dragon/leetcode/LeetCode461.java
@@ -2,19 +2,29 @@ package com.dragon.leetcode;
 
 public class LeetCode461 {
     public static int hammingDistance(int x, int y) {
-        int n = x ^ y;
-        int count = 0;
-        String str = Integer.toBinaryString(n);
-        for (int i = 0; i < str.length(); i++) {
-            if (((int)str.charAt(i)) == 49) {
-                count++;
-            }
-        }
+        return Integer.bitCount(x ^ y);
+    }
 
-        return count;
+    public static int totalHammingDistance(int[] nums) {
+        int total = 0;
+        int k = nums.length - 1;
+        for (int i = 0; i < k; i++) {
+            for(int j = i + 1; j < k; j++) {
+                total += hammingDistance(nums[i], nums[j]);
+            }
+
+            for(int m = k - 1; m > i; m--) {
+                total += hammingDistance(nums[m], nums[k]);
+
+            }
+            total += hammingDistance(nums[i], nums[k]);
+            k--;
+        }
+        return total;
     }
 
     public static void main(String[] args) {
-        System.out.println(hammingDistance(100, 10000000));
+        int[] nums = {4, 14, 2};
+        System.out.println(totalHammingDistance(nums));
     }
 }
